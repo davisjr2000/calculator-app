6320b6354e51b30abd92c263bdecb39a1e4da3ff
Davis JR <davisjr2000@hotmail.com>
2019-05-02 16:29:12 -0300

    Display all buttons

--- components/Calculator.js
+++ components/Calculator.js
@@ -1,24 +1,71 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Dimensions} from 'react-native'
 
 export default class Calculator extends Component {
+
+  state = {
+    number: 0,
+    previousNumber: 0,
+  }
+
+  numbersList = () => {
+    return [...Array(9).keys()].map((number) => {
+      return (
+        <View key={number} style={styles.button}>
+          <Text style={styles.buttonText}>{number}</Text>
+        </View>
+        )
+    })
+  }
+
   render() {
     return (
       <View>
-        <View style={styles.button}>
-          <Text>Hello World!</Text>
+        <View style={styles.input}>
+          <TextInput />
         </View>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>+</Text>
+        </View>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>-</Text>
+        </View>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>*</Text>
+        </View>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>/</Text>
+        </View>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>=</Text>
+        </View>
+        {this.numbersList()}
       </View>
       )
   }
 }
+
+const width = Dimensions.get('window').width
 
 const styles = StyleSheet.create({
   button: {
     padding: 10,
     borderColor: "black",
     borderWidth: 2,
+    height: 70,
+    width: 70,
+  },
+  buttonText: {
+    fontSize: 50,
+        textAlign: "center",
+  },
+  input: {
+    padding: 20,
+    fontSize: 20,
+    borderColor: "black",
+    borderWidth: 2,
     justifyContent: "center",
-    height: 42
+    alignSelf: 'stretch',
+    width: width
   }
 })
