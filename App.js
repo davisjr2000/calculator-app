700ef4fbafeef2a152cbb807599874eeb036ecb3
Davis JR <davisjr2000@hotmail.com>
2019-05-02 23:48:37 -0300

    Complete the app

--- App.js
+++ App.js
@@ -9,20 +9,30 @@ import InputNumberButton from './components/InputNumberButton'
 
 const buttons = [
   ['CLEAR', 'DEL'],
-  [7, 8 ,9, '/'],
-  [4, 5, 6, "x"],
-  [1, 2, 3, "-"],
-  [0, ".", "=", "+"]
+  ["7", "8" ,"9", '/'],
+  ["4", "5", "6", "x"],
+  ["1", "2", "3", "-"],
+  ["0", ".", "=", "+"]
 ];
 
 export default class App extends Component {
 
+  constructor() {
+    super()
+    this.state = {
+      displayValue: '0',
+      operator: null,
+      firstValue: '',
+      secondValue: '',
+      nextValue: false
+    }
+  }
   renderButtons() {
     let layouts = buttons.map((buttonRows, index) => {
       let rowItem = buttonRows.map((buttonItems, buttonIndex) => {
         return <InputNumberButton
           value={buttonItems}
-          handleOnPress={() => {}}
+          handleOnPress={this.handleInput.bind(this, buttonItems)}
           key={'btn-' + buttonIndex}
           />
 
@@ -33,11 +43,86 @@ export default class App extends Component {
     return layouts
   }
 
+  handleInput = (input) => {
+    const { displayValue, operator, firstValue, secondValue, nextValue } = this.state
+    switch (input) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        this.setState({
+          displayValue: displayValue === "0" ? input : displayValue + input
+        })
+
+        if (!nextValue){
+          this.setState({
+            firstValue: firstValue + input
+          })
+        } else {
+          this.setState({
+            secondValue: secondValue + input
+          })
+
+        }
+        break;
+      case '+':
+      case '-':
+      case 'x':
+      case '/':
+        this.setState({
+          operator: input,
+          displayValue: (operator !== null ? displayValue.substr(0, displayValue.length - 1) : displayValue) + input,
+          nextValue: true
+        })
+        break;
+      case '=':
+      let formatOperator = ( operator == 'x' ) ? '*' : operator
+      let result = eval(firstValue + formatOperator + secondValue)
+        this.setState({
+          displayValue: result % 1 === 0 ? result : result.toFixed(2),
+          firstValue: result % 1 === 0 ? result : result.toFixed(2),
+          secondValue: '',
+          operator: null,
+          nextValue: false,
+        })
+        break;
+      case 'CLEAR':
+        this.setState({
+          operator: null,
+          displayValue: '0'
+        })
+        break;
+      case 'DEL':
+        let string = displayValue.toString();
+        let length = string.length
+        this.setState({
+          displayValue: length == 1 ? "0" : displayValue.slice(0, -1),
+          firstValue: length == 1 ? "" : displayValue.slice(0, -1),
+
+        })
+        break;
+      case '.':
+        let dot = displayValue.toString().slice(-1)
+        this.setState({
+          displayValue: dot !== "." ?  displayValue + input : displayValue,
+        })
+        break;
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.resultContainer}>
-          <Text style={styles.resultText}>0</Text>
+          <Text style={styles.resultText}>
+            {this.state.displayValue}
+          </Text>
         </View>
         <View style={styles.inputContainer}>
           {this.renderButtons()}
